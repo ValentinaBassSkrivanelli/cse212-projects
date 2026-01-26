0c2349dc1ae8a9aaf51a2266af8bcb3088de5e46
Valentina Bass Skrivanelli <aldanavalentinabass@gmail.com>
2026-01-26 12:27:53 -0300

    WEEK4Code

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -1,24 +1,24 @@
 ﻿/*
- * CSE 212 Lesson 6C 
- * 
- * This code will analyze the NBA basketball data and create a table showing
- * the players with the top 10 career points.
- * 
- * Note about columns:
- * - Player ID is in column 0
- * - Points is in column 8
- * 
- * Each row represents the player's stats for a single season with a single team.
- */
-
+* CSE 212 Lesson 6C
+*
+* This code will analyze the NBA basketball data and create a table showing
+* the players with the top 10 career points.
+*
+* Note about columns:
+* - Player ID is in column 0
+* - Points is in column 8
+*
+* Each row represents the player's stats for a single season with a single team.
+*/
+ 
 using Microsoft.VisualBasic.FileIO;
-
+ 
 public class Basketball
 {
     public static void Run()
     {
         var players = new Dictionary<string, int>();
-
+ 
         using var reader = new TextFieldParser("basketball.csv");
         reader.TextFieldType = FieldType.Delimited;
         reader.SetDelimiters(",");
@@ -27,10 +27,26 @@ public class Basketball
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+ 
+            if (players.ContainsKey(playerId))
+            {
+                players[playerId] += points;
+            } else
+            {
+                players[playerId] = points;
+            }
+ 
         }
-
-        Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
-
-        var topPlayers = new string[10];
+ 
+        // var topplayer = new Dictionary<string, int>();
+        KeyValuePair<string, int>[] playerArray = players.ToArray();
+        // var topPlayers = players.ToArray();
+ 
+        Array.Sort(playerArray, (x, y) => y.Value.CompareTo(x.Value));
+       
+       for (int i = 0; i < 10; i++)
+       Console.WriteLine(
+                $"Rank {i + 1}: Player {playerArray[i].Key} - {playerArray[i].Value} points"
+            );
     }
 }
--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -29,5 +29,41 @@
     /// <param name="numbers">array of integers</param>
     private static void DisplaySumPairs(int[] numbers) {
         // TODO Problem 2 - This should print pairs of numbers in the given array
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // foreach (int x in numbers) {
+    //   int complement = 10 - x;
+
+    //    if (seen.Contains(complement)) {
+    //        Console.WriteLine($"{x} {complement}");
+    //    }
+
+    //   seen.Add(x);
